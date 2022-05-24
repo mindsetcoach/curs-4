2924cf29345f2af9fbf2a2621ae0b585d00eb7a3
Tudor Goia <mindsetcoach360@gmail.com>
2022-05-24 22:05:32 +0300

    the usage of axios

---
+++ index/main.js
@@ -0,0 +1,17 @@
+
+let capsulesData = null;
+
+let targetDiv = document.querySelector('div.pageContent');
+
+axios
+    .get('https://api.spacexdata.com/v4/capsules')
+    .then(function (response) {
+        // handle success
+        capsulesData = response.data;
+        targetDiv.innerHTML = JSON.stringify(capsulesData);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+;
