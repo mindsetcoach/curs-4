d4cc219658a3ec00363ac1923271fa7d13c07b29
Tudor Goia <mindsetcoach360@gmail.com>
2022-07-05 21:39:55 +0300

    used leaflet to show a map in the company map section

--- index/companyMap.js
+++ index/companyMap.js
@@ -1,12 +1,31 @@
 window.index.companyMap = {
 
+    map: null,
+
     render: function() {
-        let companyMap = document.querySelector('.pageContent > .si-companyMap');
-        companyMap.innerHTML =
-            companyMap.innerHTML +
-            '<br>we get ' +
-            index.data.allLaunchpadItems.length +
-            ' launch pads'
-        ;
+
+        index.companyMap.map = L.map('companyMap').setView([35, 10], 2);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        }).addTo(
+            index.companyMap.map
+        );
+
+        index.companyMap.setLaunchpadsLocations();
+    },
+
+    setLaunchpadsLocations: function() {
+        let allLaunchpads = index.data.allLaunchpadItems;
+
+        for (let i = 0; i < allLaunchpads.length; i++) {
+
+            let launchpad = allLaunchpads[i];
+
+            L.marker([launchpad.latitude, launchpad.longitude]).addTo(
+                index.companyMap.map
+            );
+        }
     }
 };
