f3beb414735389c5b35e211d86b56d8d4ceb5fe7
Tudor Goia <mindsetcoach360@gmail.com>
2022-07-26 20:22:07 +0300

    implemented the gallery

--- crew/crewGallery.js
+++ crew/crewGallery.js
@@ -1,9 +1,27 @@
 
 window.crew.crewGallery = {
 
-    buildGallery: function() {
+    buildGallery: function(popupClass) {
 
         let allItems = crew.data.allItems;
+
+        let itemsParent = document.querySelector('.' + popupClass + ' .modal-body');
+
+        for (let i = 0; i < allItems.length; i++) {
+            let divElement = document.createElement('div');
+            let imgElement = document.createElement('img');
+            let nameElement = document.createElement('div');
+
+            divElement.classList.add('galleryItem');
+            imgElement.setAttribute('src', allItems[i].image);
+            imgElement.classList.add('galleryPhoto');
+            nameElement.innerHTML = allItems[i].name;
+
+            divElement.appendChild(imgElement);
+            divElement.appendChild(nameElement);
+
+            itemsParent.appendChild(divElement);
+        }
     },
 
     buildCarousel: function() {
@@ -23,6 +41,6 @@ window.crew.crewGallery = {
             .querySelector('div.modal.' + popupClass)
             .style.display = 'block';
 
-        crew.crewGallery.buildGallery();
+        crew.crewGallery.buildGallery(popupClass);
     }
 }
